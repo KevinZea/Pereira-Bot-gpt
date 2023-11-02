1803f710fadb575d025b93d1635ec80a00f2ff0f
Kevin <kevinstevenzeasuarez@gmail.com>
2023-11-02 12:28:04 -0500

    change view responsive background

--- src/App.jsx
+++ src/App.jsx
@@ -164,8 +164,8 @@ function App() {
 
         </div>
         <div className='body-container'>
+          <div className='body-image'></div>
           <div className='body-chat' ref={chatContainerRef}>
-            <div className='body-image'></div>
             {chats.length > 0 && (
               chats.map((c) => {
                 return (
@@ -227,7 +227,7 @@ function App() {
       </div>
       <AddToHomeScreenButton />
     </div>
-   
+
   );
 }
 
